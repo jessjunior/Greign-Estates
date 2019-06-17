af42e1cfc01dd762e210eebb7f1e4fc2ce7b1a59
Raj Gami <raj.gami@strathmore.edu>
2019-06-17 09:38:06 +0300

    About Us Page

--- about.php
+++ about.php
@@ -33,5 +33,69 @@
 		</div>
         </div>
 </header>
+
+<div class="page-head"> 
+            <div class="container">
+                <div class="row">
+                    <div class="page-head-content">
+                        <h1 class="page-title">About Us</h1>               
+                    </div>
+                </div>
+            </div>
+        </div>
+<div class="content-area single-property" style="background-color: #FCFCFC;">&nbsp;
+            <div class="container">   
+
+                <div class="clearfix padding-top-40" >
+
+                    <div class="col-md-8 single-property-content prp-style-1 ">
+                        
+
+                        <div class="single-property-wrapper">
+                            
+                            <div class="center"><img src="img/about.jpg"></div>
+
+                            
+                            <!-- .property-meta -->
+
+                            <div class="section">
+                                
+                                <div class="s-property-content">
+                                    <p>Greign Estates Limited is a professional Valuation and Real Estate Consultancy Company operating in accordance with the Valuer’s Act Chapter 532 and Estate Agents Act Chapter 533 of the Laws of Kenya. The Company was established in 2005 and incorporated in early 2010. It is well represented in the country with it’s Headquarters in Nairobi and a branch in Nyeri, Mombasa and Kiambu. The leading consultants in the company are registered with the bodies regulating the activities of Valuers and Estate Agency Registration Boards which are the statutory bodies regulating the activities of Valuers and Estate Agents in the country as per the requirements of the Valuers and Estate Agents Acts of the Laws of Kenya. They are also Full Members of the Institution of Surveyors of Kenya (I.S.K).
+
+This Institution, mainly through the Valuation and Estate Management Surveyors (V.E.M.S.) Chapter, governs our professional activities. Indeed the valuation standards in this country are outlined in the Valuers Handbook (Pages 118-132) which is based on the Internationally accepted standards as stipulated by the International Valution Standards Committee (I.V.S.C.), which we all subscribe to.
+
+The Real Estate Consultancy Services which is the core of our business can be categorized as follows: -
+
+Valuation of both movable and immovable assets including plant and machinery;
+Management of both residential and commercial properties;
+Real estate agency including sales and letting;
+General estate consultancy including feasibilities studies, rental surveys, condition surveys among others.</p>
+                                </div>
+                            </div>
+                            <!-- End description area  -->
+
+                            
+
+                         
+                            <!-- End features area  -->
+
+                           
+                            <!-- End video area  -->
+                            
+                            
+
+                            
+                            <!-- End video area  -->
+                            
+                        </div>
+                    </div>
+
+
+                    
+                </div>
+
+            </div>
+        </div>
 </body>
 </html>
